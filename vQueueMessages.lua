f756c9430fa4d8d61bbc0b685fe4ce0a7ca2de88
rqnn <michel.fabian0210@gmail.com>
2019-10-12 11:34:59 +0200

    added german translations

--- vQueueMessages.lua
+++ vQueueMessages.lua
@@ -1,3 +1,4 @@
+-- EN
 LFMARGS = {
 	"lfm",
 	"need",
@@ -369,3 +370,420 @@ MINLVLS["lbrs"] = 55
 MINLVLS["dm"] = 56
 MINLVLS["strat"] = 58
 MINLVLS["scholo"] = 58
+
+-- DE
+DE_LFMARGS = {
+	"lfm",
+	"gesucht",
+	"suchen",
+	"lf",
+	"spot",
+	"platz",
+	"looking",
+	"lftank",
+	"lfheal",
+	"lfheals",
+	"lfdps",
+	"needed",
+	"noch jemand für"
+}
+DE_LFGARGS = {
+	"lfg",
+	"group",
+	"groups",
+	"gruppe",
+	"grp",
+	"looking",
+	"suche",
+	"gesucht",
+	"any group",
+	"any groups",
+	"any parties",
+	"any party",
+	"any partys",
+	"any one for",
+	"for hire"
+}
+DE_ROLEARGS = {
+	
+}
+DE_ROLEARGS["Healer"] = {
+	"heals",
+	"healss",
+	"heal",
+	"healer",
+	"healers",
+	"disc",
+	"resto",
+	"restoration",
+	"holy",
+	"discipline",
+	"tree",
+	"mainheal",
+	"mainhealer",
+	"mainheals",
+	"offheal",
+	"offhealer",
+	"offheals",
+	"raidheal",
+	"raidheals",
+	"raidhealer",
+	"tankheals",
+	"tankheal",
+	"tankhealer"
+	"heiler"
+}
+DE_ROLEARGS["Damage"] = {
+	"dps",
+	"damage",
+	"deeps",
+	"hunter",
+	"hunters",
+	"hunt",
+	"hun",
+	"mage",
+	"mages",
+	"rogue",
+	"rogues",
+	"warlock",
+	"lock",
+	"warlocks",
+	"wl",
+	"destro",
+	"destruction",
+	"afflic",
+	"affliction",
+	"arms",
+	"fury",
+	"enhance",
+	"enhancement",
+	"elemental",
+	"ele",
+	"retribution",
+	"ret",
+	"shadow",
+	"feral",
+	"cat",
+	"kitty",
+	"caster",
+	"melee",
+	"range",
+	"melee",
+	"mele",
+	"ranger",
+	"dd",
+	"ranged",
+	"rdps",
+	"mdps",
+	"cdps",
+	"dds",
+	"retri",
+	"range",
+	"nahkampf",
+	"fernkampf",
+}
+DE_ROLEARGS["Tank"] = {
+	"tank",
+	"prot",
+	"protection",
+	"guardian",
+	"bear",
+	"maintank",
+	"offtank"
+}
+
+DE_CATARGS = {
+	
+}
+
+DE_CATARGS["rfc"] = {
+	"ragefire chasm",
+	"ragefire",
+	"rfc"
+}
+DE_CATARGS["dead"] = {
+	"deadmines",
+	"deadmine",
+	"dm",
+	"todesminen"
+}
+DE_CATARGS["wc"] = {
+	"wailing caverns",
+	"wailing",
+	"caverns",
+	"wc",
+	"hdw",
+	"höhlen des wehklagens",
+	"höhle des wehklagens"
+}
+DE_CATARGS["sfk"] = {
+	"shadowfang keep",
+	"shadowfang",
+	"sfk",
+	"burg schattenfang",
+	"bsf"
+}
+DE_CATARGS["stock"] = {
+	"the stockade",
+	"stockade",
+	"stock",
+	"stocks",
+	"verliess",
+	"verlies",
+	"verließ"
+}
+DE_CATARGS["bfd"] = {
+	"blackfathom deeps",
+	"blackfathom",
+	"bfd",
+	"blackfathom tiefe",
+	"bft"
+}
+DE_CATARGS["gnomer"] = {
+	"gnomeregan",
+	"gnomergan",
+	"gnomer",
+	"gnome"
+}
+DE_CATARGS["rfk"] = {
+	"razorfen kraul",
+	"razerfen kraul",
+	"rfk",
+	"kral von razorfen",
+	"kral"
+}
+DE_CATARGS["graveyard"] = {
+	"graveyard",
+	"grave",
+	"yard",
+	"gy",
+	"smgy",
+	"smgraveyard",
+	"smgrave",
+	"sm all",
+	"sm any",
+	"friedhof",
+	"fh"
+}
+DE_CATARGS["library"] = {
+	"library",
+	"lib",
+	"libs",
+	"smlib",
+	"smlibrary",
+	"smlibs",
+	"bibliothek",
+	"biblio",
+	"bib"
+}
+DE_CATARGS["armory"] = {
+	"armory",
+	"arm",
+	"arms",
+	"amory",
+	"armoy",
+	"smarms",
+	"smarmory",
+	"smarm",
+	"smamory",
+	"armoruy",
+	"waffenkammer",
+	"wk"
+}
+DE_CATARGS["cathedral"] = {
+	"cathedral",
+	"cath",
+	"smcath",
+	"smcathedral",
+	"smcathe",
+	"cathe",
+	"kathedrale",
+	"kath"
+}
+DE_CATARGS["rfd"] = {
+	"razorfen downs",
+	"razerfen downs",
+	"razorfen down",
+	"razerfen down",
+	"rfd",
+	"hügel von razorfen"
+}
+DE_CATARGS["ulda"] = {
+	"uldaman",
+	"ulda",
+	"uld",
+}
+DE_CATARGS["zf"] = {
+	"zul farrak",
+	"zulfarak",
+	"zul farak",
+	"zulfarrak",
+	"zf",
+	"zul\'farrak'"
+}
+DE_CATARGS["mara"] = {
+	"maraudon",
+	"marudon",
+	"mara",
+	"maru"
+}
+DE_CATARGS["st"] = {
+	"sunken",
+	"sunk",
+	"sunkentemple",
+	"st",
+	"versunkener tempel",
+	"tempel",
+	"temple"
+}
+DE_CATARGS["brd"] = {
+	"blackrock depths",
+	"blackrock dpths",
+	"brd",
+	"lava run",
+	"arena run",
+	"vault run"
+}
+DE_CATARGS["lbrs"] = {
+	"lower brs",
+	"lower blackrock",
+	"lbrs",
+	"untere blackrockspitze"
+}
+DE_CATARGS["dm"] = {
+	"dire",
+	"maul",
+	"dm",
+	"tribute",
+	"trib",
+	"düsterbruch"
+}
+DE_CATARGS["strat"] = {
+	"stratholme",
+	"strath",
+	"strat"
+}
+DE_CATARGS["scholo"] = {
+	"scholomance",
+	"scholo",
+	"schol"
+}
+DE_CATARGS["ubrs"] = {
+	"upper",
+	"ubrs",
+	"obere blackrockspitze"
+}
+DE_CATARGS["ony"] = {
+	"ony",
+	"onyxia",
+	"onyxias"
+}
+DE_CATARGS["zg"] = {
+	"zg",
+	"zul\'g",
+	"zul\'gurub",
+	"gurub"
+}
+DE_CATARGS["mc"] = {
+	"molten",
+	"core",
+	"mc",
+	"rag",
+	"ragnaros",
+	"geschmolzener kern"
+}
+DE_CATARGS["ruins"] = {
+	"aq",
+	"ahn",
+	"qiraj",
+	"ruins"
+}
+DE_CATARGS["temple"] = {
+	"aq",
+	"ahn",
+	"qiraj",
+	"temple"
+}
+DE_CATARGS["bwl"] = {
+	"blackwing",
+	"blacwing",
+	"bwl",
+	"pechschwingen"
+}
+DE_CATARGS["naxx"] = {
+	"naxx",
+	"naxxramas",
+	"nax"
+}
+DE_CATARGS["wsg"] = {
+	"wsg",
+	"warsong",
+	"gulch",
+	"warsongschlucht"
+}
+DE_CATARGS["ab"] = {
+	"ab",
+	"arathi",
+	"basin",
+	"arati",
+	"arathibecken"
+}
+DE_CATARGS["av"] = {
+	"av",
+	"alterac",
+	"valley",
+	"aterac",
+	"alteractal"
+}
+
+DE_MINLVLS = {
+	
+}
+
+DE_MINLVLS["quest110"] = 1
+DE_MINLVLS["quest1020"] = 10
+DE_MINLVLS["quest2030"] = 20
+DE_MINLVLS["quest3040"] = 30
+DE_MINLVLS["quest4050"] = 40
+DE_MINLVLS["quest5060"] = 50
+
+DE_MINLVLS["misc"] = 1
+
+-- bgs --
+DE_MINLVLS["wsg"] = 10
+DE_MINLVLS["ab"] = 20
+DE_MINLVLS["av"] = 51
+
+-- raids --
+DE_MINLVLS["ubrs"] = 58
+DE_MINLVLS["ony"] = 60
+DE_MINLVLS["zg"] = 60
+DE_MINLVLS["mc"] = 60
+DE_MINLVLS["ruins"] = 60
+DE_MINLVLS["bwl"] = 60
+DE_MINLVLS["temple"] = 60
+DE_MINLVLS["naxx"] = 60
+
+-- dungeons --
+DE_MINLVLS["rfc"] = 13
+DE_MINLVLS["dead"] = 17
+DE_MINLVLS["wc"] = 17
+DE_MINLVLS["sfk"] = 22
+DE_MINLVLS["stock"] = 23
+DE_MINLVLS["bfd"] = 23
+DE_MINLVLS["gnomer"] = 29
+DE_MINLVLS["rfk"] = 29
+DE_MINLVLS["graveyard"] = 29
+DE_MINLVLS["library"] = 32
+DE_MINLVLS["armory"] = 35
+DE_MINLVLS["cathedral"] = 37
+DE_MINLVLS["rfd"] = 37
+DE_MINLVLS["ulda"] = 41
+DE_MINLVLS["zf"] = 43
+DE_MINLVLS["mara"] = 46
+DE_MINLVLS["st"] = 49
+DE_MINLVLS["brd"] = 50
+DE_MINLVLS["lbrs"] = 55
+DE_MINLVLS["dm"] = 56
+DE_MINLVLS["strat"] = 58
+DE_MINLVLS["scholo"] = 58
